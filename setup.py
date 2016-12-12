84cccb7fbdd11fae36aa60b3941a445be78a3970
snowwxradar <robertschrom2@gmail.com>
2016-12-12 15:21:20 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='radar_simulate',
+      version='0.1',
+      description='Functions to simulate radar variables from particle size distributions',
+      url='https://github.com',
+      author='Robert Schrom',
+      author_email='rss5116@psu.edu',
+      license='?',
+      packages=['radar_simulate'],
+      zip_safe=False)
