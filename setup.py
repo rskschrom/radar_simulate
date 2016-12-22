0fa808535ed19b6032fd30ef6ada8d67bebf112e
snowwxradar <snowwxradar@users.noreply.github.com>
2016-12-22 15:42:10 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='radar_simulate',
       version='0.1',
       description='Functions to simulate radar variables from particle size distributions',
-      url='https://github.com',
+      url='https://github.com/snowwxradar/radar_simulate',
       author='Robert Schrom',
       author_email='rss5116@psu.edu',
       license='?',
