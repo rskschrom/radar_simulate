ef40ad6b1c7a558538a2833a97d8dac459793c3b
snowwxradar <snowwxradar@users.noreply.github.com>
2017-01-16 14:25:59 -0500

    Update constants.py

--- radar_simulate/constants.py
+++ radar_simulate/constants.py
@@ -1,5 +1,8 @@
 # definitions of useful constants
 eps_ice_x = complex(3.16835, 0.0089)
+eps_ice_s = complex(3.16835, 0.02492)
 eps_liq_x = complex(44.593, 41.449)
+eps_liq_s = complex(80.255, 24.313)
 wavl_x = 32.1
+wavl_s = 100.
 kw2 = 0.93
